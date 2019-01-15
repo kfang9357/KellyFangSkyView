e95abcb4ac2dcf2befceba5f270a42e44ecad2a8
BT_1N3_27@bktech.local <BT_1N3_27@bktech.local>
2019-01-15 08:37:54 -0500

    finished constructor; onto getAvg method

---
+++ src/com/company/SkyView.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class SkyView {
+
+
+    public SkyView (int numRows, int numCols, double[] scanned)
+    {
+        double [][] view = new double[numRows][numCols];
+
+        int k = 0;
+        int len = scanned.length;
+
+        while (k< len)
+        {
+            for (int j = 0; j< numRows; j++)
+            {
+                if (j%2<1)
+                {
+                    for (int i = 0; i< numCols; i++)
+                    {
+                        view[j][i] = scanned[k];
+                        k++;
+                    }
+                }
+                else
+                {
+                    for (int i = numCols; i>0; i--)
+                    {
+                        view[j][i] = scanned[k];
+                    }
+                }
+
+            }
+        }
+    }
+
+    public double getAverage (int startRow, int endRow, int startCol, int endCol)
+    {
+        
+    }
+
+
+
+}
