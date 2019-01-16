c053926c8c27104b70d99c6c2457649953a8c77b
BT_1N3_27 <kfang9357@bths.edu>
2019-01-16 08:39:05 -0500

    need to fix getAvg

--- src/com/company/SkyView.java
+++ src/com/company/SkyView.java
@@ -9,44 +9,32 @@ public class SkyView {
         double [][] view = new double[numRows][numCols];
 
         int k = 0;
-        int len = scanned.length;
-
-        while (k< len)
-        {
             for (int j = 0; j< numRows; j++)
             {
-                if (j%2<1)
+                for (int i = 0; i<numCols; i++)
                 {
-                    for (int i = 0; i< numCols; i++)
-                    {
+                    if (j%2==1){
                         view[j][i] = scanned[k];
                     }
-                }
-                else
-                {
-                    for (int i = numCols; i>0; i--)
+                    else
                     {
                         view[j][i] = scanned[k];
-
                     }
                 }
                 k++;
+            }
 
+    }
+    public double getAverage (int startRow, int endRow, int startCol, int endCol) {
+        double sum = 0;
+        int k = 0;
+        for (int i = startRow; i <= endRow; i++) {
+            for (int j = startCol; j <= endCol; j++) {
+                sum = sum + view[i][j];
+                k++;
             }
         }
+        return sum / k;
     }
-
-    public double getAverage (int startRow, int endRow, int startCol, int endCol)
-    {
-        double sum = 0;
-        for (int i = startRow; i<endRow; startRow++)
-            for (int j = startCol; j< endCol; startCol++)
-            {
-                sum = sum + view [i][j];
-            }
-            return sum;
-    }
-
-
 
 }
